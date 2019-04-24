5b9c5018e6e5440ce957d433271a2403ef693c97
bhargavikumar13 <bhargavikumar13@gmail.com>
2019-04-24 23:51:08 +0530

    adding urls change

--- webf/urls.py
+++ webf/urls.py
@@ -18,7 +18,9 @@ from django.urls import path,include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('pbh.urls')),
     path('pbh/',include('pbh.urls')),
+     
 
 ]
 
